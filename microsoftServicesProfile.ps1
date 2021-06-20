0031aa59e224ac75f543fb4c945a51e430382eca
Nik <nikkelly@outlook.com>
2021-06-20 11:27:56 -0700

    Added orgname prompt when connecting to SharePoint

--- microsoftServicesProfile.ps1
+++ microsoftServicesProfile.ps1
@@ -19,9 +19,9 @@ else {
   Write-Host " (Y / N)" -ForegroundColor White -NoNewLine
   $saveCreds = Read-Host -Prompt " "
   $result = Switch ($saveCreds) {
-    Y {$true }
-    N {$false }
-    Default {$false }
+    Y { $true }
+    N { $false }
+    Default { $false }
   }
   # convert username to encrypted string
   $secureUser = ConvertTo-SecureString $microsoftUser -AsPlainText -Force
@@ -33,16 +33,15 @@ else {
     # save credentials as a User scoped environment variable  
     [System.Environment]::SetEnvironmentVariable('microsoftConnectionUser', $encryptedUser, [System.EnvironmentVariableTarget]::User)
     [System.Environment]::SetEnvironmentVariable('microsoftConnectionPass', $microsoftPassword, [System.EnvironmentVariableTarget]::User)
-    }
   }
+}
 # create microsoftCreds with user + pass
 # convert encrytped string password > SecureString > PlainText
 $secureUser = ConvertTo-SecureString $encryptedUser
-$microsoftUser =[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureUser))
+$microsoftUser = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureUser))
 # Convert encrypted string password back to secureString
 $global:securePwd = ConvertTo-SecureString $microsoftPassword
 $global:creds = New-Object System.Management.Automation.PSCredential -ArgumentList $microsoftUser, $securePwd
-$orgName = $microsoftUser.split('@').split('.')[1] # split the domain from $microsoftUser
 $domain = $microsoftUser.split('@')[1]
 
 # display found account
@@ -53,7 +52,8 @@ if (Test-Path  env:microsoftConnectionMFA ) {
   Write-Host "MFA status: " -ForegroundColor Yellow -NoNewLine
   Write-Host "Enabled`n" -ForegroundColor Green -NoNewLine
   $script:mfaCheck = $TRUE
-} else {
+}
+else {
   Write-Host "MFA status: " -ForegroundColor Yellow -NoNewLine
   Write-host "Disabled`n" -ForegroundColor Red -NoNewLine
 }
@@ -69,8 +69,6 @@ Write-Host '$microsoftUser = ' -ForeGroundColor Yellow -NoNewLine
 Write-Host $microsoftUser -ForeGroundColor White
 Write-Host '$domain = ' -ForegroundColor Yellow -NoNewLine
 Write-Host $domain -ForegroundColor White
-Write-Host '$orgName = ' -ForegroundColor Yellow -NoNewLine
-Write-Host $orgName`n`n -ForeGroundColor White
 
 
 
@@ -83,17 +81,17 @@ function global:prompt() {
     $service = $service.replace('|', '+')
   } 
 
-# Update the prompt
-if ($serviceCount -gt 0) {
-  Write-Host "$service" -ForegroundColor Yellow -NoNewline 
-  Write-Host " | " -ForegroundColor  White -NoNewline 
-  Write-Host ('' + $(Get-Location) + ">")  -NoNewLine 
-}
-else {
-  Write-Host ("$service" + $(Get-Location) + ">") -NoNewLine `
+  # Update the prompt
+  if ($serviceCount -gt 0) {
+    Write-Host "$service" -ForegroundColor Yellow -NoNewline 
+    Write-Host " | " -ForegroundColor  White -NoNewline 
+    Write-Host ('' + $(Get-Location) + ">")  -NoNewLine 
+  }
+  else {
+    Write-Host ("$service" + $(Get-Location) + ">") -NoNewLine `
 
-}
-return " "
+  }
+  return " "
 }
 
 
@@ -151,17 +149,17 @@ function connectAll() {
   MSOnline
 }
 
-function checkInstallModule($moduleName){ 
-  $installModule = Switch($moduleName){
-    Teams {'MicrosoftTeams'}
-    Exchange {'ExchangeOnlineManagement'}
-    SharePoint {'Microsoft.Online.SharePoint.PowerShell'}
-    AzureAD {'AzureAD'}
-    MSOnline {'MSOnline'}
-    Security_Compliance {'ExchangeOnlineManagement'}
+function checkInstallModule($moduleName) { 
+  $installModule = Switch ($moduleName) {
+    Teams { 'MicrosoftTeams' }
+    Exchange { 'ExchangeOnlineManagement' }
+    SharePoint { 'Microsoft.Online.SharePoint.PowerShell' }
+    AzureAD { 'AzureAD' }
+    MSOnline { 'MSOnline' }
+    Security_Compliance { 'ExchangeOnlineManagement' }
   }
   # If module is not already installed, prompt for install 
-  if(!(Get-Module -ListAvailable -Name $installModule)){
+  if (!(Get-Module -ListAvailable -Name $installModule)) {
     Write-Host "Module " -NoNewLine
     Write-Host "$installModule " -ForegroundColor Yellow -NoNewLine
     Write-Host "is missing, would you like to install it? (Y / N)" -NoNewLine
@@ -173,27 +171,30 @@ function checkInstallModule($moduleName){
     }
     if ($installAnswer -eq $true) {
       # Check if admin
-      try{
+      try {
         if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-        # Run with -AllowClobber for AzureAD
-        if($installModule -eq 'AzureAD'){
-          Install-Module -Repository "PSGallery" -Name $installModule -AllowClobber -Force
-        } else {
-          Install-Module -Repository "PSGallery" -Name $installModule -Force
+          # Run with -AllowClobber for AzureAD
+          if ($installModule -eq 'AzureAD') {
+            Install-Module -Repository "PSGallery" -Name $installModule -AllowClobber -Force
+          }
+          else {
+            Install-Module -Repository "PSGallery" -Name $installModule -Force
+          }
+        }
+        else {
+          Write-Host "`nAdministrator rights are required to install modules. Prompting for rights..." 
+          # pass install to an elevated PowerShell window
+          # $runCommand = Install-Module $installModule -Repository "PSGallery"
+          if ($installModule -eq 'AzureAD') {
+            $runCommand = "Install-Module -Repository 'PSGallery' -Name $installModule -Force -AllowClobber"
+          }
+          else {
+            $runCommand = "Install-Module -Repository 'PSGallery' -Name $installModule -Force"
+          }
+          start-process -filepath powershell.exe -argumentlist @('-command', $runCommand) -verb runas -wait
         }
       }
-      else {
-        Write-Host "`nAdministrator rights are required to install modules. Prompting for rights..." 
-        # pass install to an elevated PowerShell window
-        # $runCommand = Install-Module $installModule -Repository "PSGallery"
-        if($installModule -eq 'AzureAD'){
-          $runCommand = "Install-Module -Repository 'PSGallery' -Name $installModule -Force -AllowClobber"
-        } else {
-        $runCommand = "Install-Module -Repository 'PSGallery' -Name $installModule -Force"
-        }
-        start-process -filepath powershell.exe -argumentlist @('-command',$runCommand) -verb runas -wait
-      }
-      } catch {
+      catch {
         Write-Host "Error during install, please try again."
       }
    
@@ -255,14 +256,23 @@ function MSOnline() {
 function SharePoint() {
   checkServices($MyInvocation.MyCommand.name)
   checkInstallModule($MyInvocation.MyCommand.name)
+  Write-Host "Prompting for SharePoint organization name"
+  Write-Host "Example " -ForegroundColor Yellow -NoNewline
+  Write-Host "https://tenantname-admin.sharepoint.com" -ForegroundColor White
+  Write-host "`n`n"
+  $orgName = Read-Host -Prompt "Organization name"
+  # if $orgName contains -admin this should remove it
+  if ($orgName -like '*-admin') {
+    $orgName = $orgName.split('-')[0]
+  }
   if ($script:alreadyConnected = 1) {
     if ($mfaCheck) {
       Connect-SPOService -Url https://$orgName-admin.sharepoint.com
+      else {
+        Connect-SPOService -Url https://$orgName-admin.sharepoint.com -Credential $creds
+      }
+      Increment($MyInvocation.MyCommand.name)
     }
-    else {
-      Connect-SPOService -Url https://$orgName-admin.sharepoint.com -Credential $creds
-    }
-    Increment($MyInvocation.MyCommand.name)
   }
 }
 
@@ -314,19 +324,19 @@ function Security_Compliance() {
   }
 }
 
-function Remove-Account(){
+function Remove-Account() {
   Write-Host "`n Removing saved username, password and MFA settings from environment variables"
-  [Environment]::SetEnvironmentVariable("microsoftConnectionMFA",$null,"User")
-  [Environment]::SetEnvironmentVariable("microsoftConnectionUser",$null,"User")
-  [Environment]::SetEnvironmentVariable("microsoftConnectionPass",$null,"User")
+  [Environment]::SetEnvironmentVariable("microsoftConnectionMFA", $null, "User")
+  [Environment]::SetEnvironmentVariable("microsoftConnectionUser", $null, "User")
+  [Environment]::SetEnvironmentVariable("microsoftConnectionPass", $null, "User")
 }
 
-function Add-MFA(){
+function Add-MFA() {
   Write-Host "Saving MFA settings to environment variable"
   [System.Environment]::SetEnvironmentVariable('microsoftConnectionMFA', $true, [System.EnvironmentVariableTarget]::User)
   $script:mfaCheck = $mfaAnswer
 }
 
-function Remove-MFA(){
-  [Environment]::SetEnvironmentVariable("microsoftConnectionMFA",$null,"User")
+function Remove-MFA() {
+  [Environment]::SetEnvironmentVariable("microsoftConnectionMFA", $null, "User")
 }
