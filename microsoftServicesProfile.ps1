3c552f7b95e6dafa2be6abc3f351d51315a6b75b
Nik Kelly <nikkelly@outlook.com>
2021-10-23 21:56:04 -0700

    adding intune support

--- microsoftServicesProfile.ps1
+++ microsoftServicesProfile.ps1
@@ -62,7 +62,7 @@ if (Test-Path  env:microsoftConnectionMFA ) {
 }
 
 Write-Host "`nConnect to Microsoft online services with these commands: " -ForegroundColor Green
-Write-Host "Teams | ExchangeServer | Exchange | MSOnline (AAD V1) | AzureAD (AAD V2) | SharePoint | Security_Compliance | connectAll | Disconnect`n" -ForegroundColor Yellow
+Write-Host "Teams | ExchangeServer | Exchange | MSOnline (AAD V1) | AzureAD (AAD V2) | SharePoint | Security_Compliance | Intune | connectAll | Disconnect`n" -ForegroundColor Yellow
 
 Write-Host "Manage Account Credentials with: " -ForegroundColor Green
 Write-Host "Remove-Account | Add-MFA | Remove-MFA `n" -ForegroundColor Yellow
@@ -150,6 +150,7 @@ function connectAll() {
   AzureAD
   MSOnline
   exchangeServer
+  Intune
 }
 
 function checkInstallModule($moduleName) { 
@@ -160,6 +161,7 @@ function checkInstallModule($moduleName) {
     AzureAD { 'AzureAD' }
     MSOnline { 'MSOnline' }
     Security_Compliance { 'ExchangeOnlineManagement' }
+    Intune {'Microsoft.Graph.Intune'}
   }
   # If module is not already installed, prompt for install 
   if (!(Get-Module -ListAvailable -Name $installModule)) {
@@ -234,6 +236,21 @@ function AzureAD() {
     }
     else {
       Connect-AzureAD -Credential $creds
+    }
+    Increment($MyInvocation.MyCommand.name)
+  }
+}
+
+# Intune
+function Intune(){
+  checkServices($MyInvocation.MyCommand.name)
+  checkInstallModule($MyInvocation.MyCommand.name)
+  if ($script:alreadyConnected = 1) {
+    try{Connect-MSGraph}
+    catch{
+      Write-host Graph Connection Failed
+      Write-Host You may need to connect with /'Connect-MSGraph -Consent'/
+      Write-Host More Info: https://github.com/Microsoft/Intune-PowerShell-SDK
     }
     Increment($MyInvocation.MyCommand.name)
   }
